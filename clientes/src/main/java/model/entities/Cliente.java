0ad3ddae0e5e480466eaadd576387cf580345114
Rodrigo Nunes <rodrigonl10.rn@gmail.com>
2020-06-26 09:10:27 -0300

    Adicionando lombok

--- clientes/src/main/java/model/entities/Cliente.java
+++ clientes/src/main/java/model/entities/Cliente.java
@@ -8,18 +8,23 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Data
 public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@Column(nullable = false, length = 150)
 	private String nome;
-	
+
 	@Column(nullable = false, length = 11)
 	private String cpf;
-	
+
 	@Column
 	private LocalDate dataCadastro;
+
 }
